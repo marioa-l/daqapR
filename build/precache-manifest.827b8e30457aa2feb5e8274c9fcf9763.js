a068eed11796cb92933c0d38aff78d0a7d854137
Mario Alejandro Leiva <mario.leiva.al@gmail.com>
2021-07-26 00:24:08 -0300

    Finished tutorial

---
+++ build/precache-manifest.827b8e30457aa2feb5e8274c9fcf9763.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef4bd0c563900925b7dc5a094065da4d",
+    "url": "/~daqap/index.html"
+  },
+  {
+    "revision": "a8969b142d041360644d",
+    "url": "/~daqap/static/css/2.7d235c77.chunk.css"
+  },
+  {
+    "revision": "2c812ef116eb3b01e9ed",
+    "url": "/~daqap/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "a8969b142d041360644d",
+    "url": "/~daqap/static/js/2.e2db7a17.chunk.js"
+  },
+  {
+    "revision": "2c05cd1498996cbd6f5332a130b13e19",
+    "url": "/~daqap/static/js/2.e2db7a17.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2c812ef116eb3b01e9ed",
+    "url": "/~daqap/static/js/main.65e00c82.chunk.js"
+  },
+  {
+    "revision": "ad090225cc4ee777f6e6",
+    "url": "/~daqap/static/js/runtime-main.cf7ae583.js"
+  },
+  {
+    "revision": "f8063e412763f24da68bc6ab8bee4116",
+    "url": "/~daqap/static/media/legend.f8063e41.png"
+  },
+  {
+    "revision": "b643793f30df778f18ec32a5abf916bc",
+    "url": "/~daqap/static/media/sectionDelpGraph.b643793f.png"
+  },
+  {
+    "revision": "b81d11c4bfcd3300ab4b6b287ba93c1e",
+    "url": "/~daqap/static/media/sectionForDungExtension.b81d11c4.png"
+  },
+  {
+    "revision": "c740f1b52001e99682377812e1b62e25",
+    "url": "/~daqap/static/media/sectionForDungGraph.c740f1b5.png"
+  },
+  {
+    "revision": "a5696edba072e6460a3f3367816865e6",
+    "url": "/~daqap/static/media/sectionProgram.a5696edb.png"
+  },
+  {
+    "revision": "55c134b44b65aebe17e89141373bfd3b",
+    "url": "/~daqap/static/media/sectionTree.55c134b4.png"
+  },
+  {
+    "revision": "25be23d22544eb4514f9f3aa2ce1b145",
+    "url": "/~daqap/static/media/sectionTree2.25be23d2.png"
+  },
+  {
+    "revision": "6d4424300c43801c7de0c5affcdd5d3d",
+    "url": "/~daqap/static/media/sectionView1.6d442430.png"
+  },
+  {
+    "revision": "d538d8276338b4d27c2ccfcfb4cf27fd",
+    "url": "/~daqap/static/media/sectionView2.d538d827.png"
+  },
+  {
+    "revision": "4015112f4133bd2ca424f3047e998035",
+    "url": "/~daqap/static/media/sectionView3.4015112f.png"
+  },
+  {
+    "revision": "a18807183cc7f8bc9721c1dd900ed760",
+    "url": "/~daqap/static/media/sectionView4.a1880718.png"
+  }
+]);
--- src/info/home.js
+++ src/info/home.js
@@ -18,8 +18,8 @@ import sectionForDungExtension from "./sectionForDungExtension.png"
 function HomeComponent() {
   return (
     <Container>
-      <h2 style={{ textAlign: 'center' }}>Defeasible Argumentation Query Answering Platform (DAQAP)</h2>
-      <p style={{ textAlign: 'justify', fontStyle: 'italic' }}>DAQAP is a web platform for
+      <h2 style={{ textAlign: 'center', paddingTop: '15px' }}>Defeasible Argumentation Query Answering Platform (DAQAP)</h2>
+      <p style={{ textAlign: 'justify', fontStyle: 'italic', paddingTop: '15px' }}>DAQAP is a web platform for
         Defeasible Argumentation Query Answering, which offers a visual interface that facilitates the analysis of the argumentative process defined in
         the Defeasible Logic Programming (DeLP) formalism.</p>
       <p style={{ textAlign: 'justify' }}>
@@ -31,35 +31,57 @@ function HomeComponent() {
           <img src={sectionDeLPGraph}></img>
         </Col>
         <Col lg='6'>
-          <p>Text for program section</p>
+          <p style={{ paddingTop: '25%', textAlign: 'justify', color: '#525252' }}>
+            The analysis for DeLP programs is handled by the DeLP
+            core module, which takes a DeLP program and a preference criterion as inputs, and returns a JSON object that contains information about: (i) the arguments
+            that can be constructed from the program, (ii) the defeat relationships, (iii) the set of warranted
+            literals as its correspond labeled tree, and (iv) the subargument relationships between arguments. From this data, a DeLP graph is drawn to show all arguments,
+            subarguments, defeat relationships, and status of each argument. The DeLP graph allows to analyze the structures and relationships that are generated from the input
+            program. In order to see the set of rules that conform a particular argument, the user can hover the mouse pointer over the argument in the graph and it will be
+            show (in a tooltip window) the set of rules that comprise it.
+
+          </p>
         </Col>
       </Row>
-      <Row style={{ borderTop: 'groove 1px', paddingBottom: '10px', paddingTop: '10px'}}>
+      <Row style={{ borderTop: 'groove 1px', paddingBottom: '10px', paddingTop: '10px', color: '#525252' }}>
         <Col lg='6'>
-          <p>Text for program legend</p>
+          <p style={{ paddingTop: '20%', textAlign: 'justify' }}>
+            In the graph, the arguments are represented as triangles with the conclusion of the argument
+            at the top and an identifier in its body. The relationships between the arguments can be 'defeat' or 'subargument'. In the particular case of the defeat, DAQAP can show if it is 'proper' or 'blocking' and if the point of attack is to the 'conclusion' or to an 'internal point'. Also, each argument is painted green if its status is 'undefeated' and red if it is a 'defeated' argument (based on its dialectic trees).
+          </p>
         </Col>
         <Col lg='6'>
-          <img src={sectionLegend}></img>
+          <img src={sectionLegend} style={{borderRadius:'15px'}}></img>
         </Col>
       </Row>
-      <Row style={{ borderTop: 'groove 1px', paddingTop: '10px' }}>
+      <Row style={{ borderTop: 'groove 1px', paddingTop: '10px', color: '#525252' }}>
         <Col lg='6'>
           <img src={sectionProgram}></img>
         </Col>
         <Col lg='6'>
-          <p>Text for program section</p>
+          <p style={{ paddingTop: '25%', textAlign: 'justify' }}>
+            This interface component is the one that allows the loading of the delp program to be analyzed. For this there are three options: enter the program rules in the text area, load some of the preloaded examples (through the selector), or load a delp program from the pc (click 'Load'). Once the program is loaded, click on 'Analyze DeLP' for the platform to analyze the program and generate the structures to be plotted.
+
+            The format for strict rules is 'Head &lt;- Body.', for defeasible rules is 'Head &gt;- Body.'  and for the facts is 'Fact.'.
+
+            The preference criteria is specified with 'use_criterion(more_specific)'. If this statement is removed, all attack relationships are considered defeats.
+          </p>
         </Col>
       </Row>
       <Row style={{ borderTop: 'groove 1px', paddingTop: '10px' }}>
         <Col lg='6'>
-          <p>Text for program section</p>
+          <p style={{ paddingTop: '35%', textAlign: 'justify', color: '#525252' }}>
+            In addition to the graph,  it is also possible to visualize the dialectical tree
+            generated for each argument. Here, it is also possible to distinguish between the 'proper' and
+            'blocking' defeat relationships.
+          </p>
         </Col>
         <Col lg='6'>
           <Row>
             <Col lg='6'><img src={sectionTree}></img></Col>
             <Col lg='6'>
-              <img src={sectionTree2} style={{ width: "40vh", paddingTop: '50%'}}></img>
-              </Col>
+              <img src={sectionTree2} style={{ width: "40vh", paddingTop: '50%' }}></img>
+            </Col>
           </Row>
         </Col>
       </Row>
@@ -75,12 +97,31 @@ function HomeComponent() {
           </Row>
         </Col>
         <Col lg='6'>
-          <p>Text for program legend</p>
+          <p style={{ paddingTop: '25%', textAlign: 'justify', color: '#525252' }}>
+            The interface is capable of showing all arguments generated from the
+            DeLP program, the defeat and subargument relationships, and the status of
+            each argument in a single graph called the DeLP graph. By default, all of this
+            data is drawn; however, since for larger programs this can generate very saturated
+            graphs that are difficult to analyze, we developed a set of different views that can
+            be configured through the “Views” panel. These views allow to hide certain
+            information, reducing the connections or hiding the final status of the arguments,
+            so the user can focus on the aspect that is most important to them.
+          </p>
         </Col>
       </Row>
       <Row style={{ borderTop: 'groove 1px', paddingTop: '10px' }}>
         <Col lg='6'>
-          <p>Text for program section</p>
+          <p style={{ paddingTop: '25%', textAlign: 'justify', color: '#525252' }}>
+            DAQAP also allows to analyze the generated DeLP
+            graph by considering it as an abstract argumentation framework, which is essentially a directed graph in which the arguments are represented by the nodes
+            and the attack relation is represented by the arcs. It is also possible to calculate
+            and display different semantics (grounded, stable, preferred and semistable) for
+            the graph. In DeLP, an attack only succeeds
+            as a defeat if the attacked argument is not preferred to the attacking argument.
+            Therefore, this defeat relation is used as the binary relation in the Dung
+            framework. Here, to create the Dung graph the classical components are used, i.e., the
+            arguments are represented by nodes and the attack relations are represented by arcs.
+          </p>
         </Col>
         <Col lg='6'>
           <img src={sectionForDungGraph}></img>
@@ -91,10 +132,19 @@ function HomeComponent() {
           <img src={sectionForDungExtension}></img>
         </Col>
         <Col lg='6'>
-          <p>Text for program section</p>
+          <p style={{ paddingTop: '25%', textAlign: 'justify', color: '#525252' }}>
+            From the generated Dung graph, the following semantics can
+            be calculated: 'grounded', 'preferred', 'stable' and 'semistable'. The calculated extensions are plotted in another graph to the right of the Dung framework. The
+            calculation of these semantics is done through the Arg-Tech solver, which
+            is provided as a Web service. Under the generated Dung graph, the semantics that can be calculated are
+            shown; when selecting any of them, the query is made to the Arg-Tech
+            Web service and the result is shown in another graph to the right of the first
+            one. Once the extensions of a semantics are calculated, it is possible to select any of them to graph the nodes that belong to it (painted in green) and those that do not belong to it (painted in red.)
+
+          </p>
         </Col>
       </Row>
-      <p style={{ textAlign: 'justify', fontStyle: 'italic' }}>[1] - Leiva, M. A., Simari, G. I., Gottifredi, S., García, A. J., & Simari, G. R. (2019, June). DAQAP: defeasible argumentation query answering platform. In International Conference on Flexible Query Answering Systems (pp. 126-138). Springer, Cham.</p>
+      <p style={{ textAlign: 'justify', fontStyle: 'italic', paddingTop: '15px' }}>[1] - Leiva, M. A., Simari, G. I., Gottifredi, S., García, A. J., & Simari, G. R. (2019, June). DAQAP: defeasible argumentation query answering platform. In International Conference on Flexible Query Answering Systems (pp. 126-138). Springer, Cham.</p>
     </Container>
 
   )
